ff6db85edf81fa7ca9eea537e05e2e92336bbd17
nasan016 <nlm040603@gmail.com>
2022-07-02 19:06:00 -0400

    initialized pinia

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
 
+const pinia = createPinia()
+
 createApp(App)
   .use(router)
+  .use(pinia)
   .use(vuetify)
   .mount('#app')
